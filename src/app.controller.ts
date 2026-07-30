036311cdd0861111e560d2d4de0690cb0c586794
nhithedev <nhitofu@gmail.com>
2026-07-30 16:17:13 +0700

    init nest + swagger + i18n

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +8,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(I18nContext.current()?.lang);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS Practice API')
+    .setDescription('API documentation for NestJS Practice tutorial')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  console.log(
+    `Swagger is available at: http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
 }
-bootstrap();
+
+void bootstrap();
